49b3f5b1cc9a1ed563b7b27ff7040976896936c5
alragi0 <140979427+alragi0@users.noreply.github.com>
2023-09-11 03:53:03 +0300

    Update collect.py

--- collect.py
+++ collect.py
@@ -37,38 +37,38 @@ async def Add_NUMBER(event, api_id, api_hash, phone_number):
         iqthon = TelegramClient("sessions/"+phone_number+".session", api_id, api_hash)
         await iqthon.connect()
         
-        if not await iqhon.is_user_authorized():
-           request = await iqhon.send_code_request(phone_number)
+        if not await iqthon.is_user_authorized():
+            request = await iqthon.send_code_request(phone_number)
         code_type = {
-        SentCodeType.APP: 'تطبيق التليجرام',
-        SentCodeType.CALL: 'مكالمه صوتيه',
-        SentCodeType.FLASH_CALL: 'مكالمه سريعه',
-        SentCodeType.SMS: 'رسائل الهاتف',
-        SentCodeType.EMAIL_CODE: 'البريد الالكتروني',
-        SentCodeType.FRAGMENT_SMS: 'التسجيل الوهمي',
-    }[code.type]
-                         async with bot.conversation(event.chat_id, timeout=300) as conv:
+            SentCodeType.APP: 'تطبيق التليجرام',
+            SentCodeType.CALL: 'مكالمه صوتيه',
+            SentCodeType.FLASH_CALL: 'مكالمه سريعه',
+            SentCodeType.SMS: 'رسائل الهاتف',
+            SentCodeType.EMAIL_CODE: 'البريد الالكتروني',
+            SentCodeType.FRAGMENT_SMS: 'التسجيل الوهمي',
+        }[code.type]
+        
+        async with bot.conversation(event.chat_id, timeout=300) as conv:
             # verification code
-                   verification_message = (
+            verification_message = (
                 f"**- تم إرسال كود التحقق عبر *{code_type}*"
                 f"\n من فضلك قم بإرساله ووضع ( - ) بين كل رقم."
                 f"\n انتظر ⏳ :**"
             )
-       try:
-            verification_code_msg = await conv.send_message(verification_message)
-            response_verification_code = await conv.get_response()
-            verification_code = str(response_verification_code.message).replace('-', '')
+            try:
+                verification_code_msg = await conv.send_message(verification_message)
+                response_verification_code = await conv.get_response()
+                verification_code = str(response_verification_code.message).replace('-', '')
 
-    except Exception as error:
-        return str(error)
-# ... (متابعة الكود)
-                try:
-                    login = await iqthon.sign_in(phone_number, code=int(verification_code))
-                except errors.SessionPasswordNeededError:
-                    password_msg = await conv.send_message("الحساب محمي بكلمة السر, ارسل كلمة السر :")
-                    password = await conv.get_response()
+            except Exception as error:
+                return str(error)
+            try:
+                login = await iqthon.sign_in(phone_number, code=int(verification_code))
+            except errors.SessionPasswordNeededError:
+                password_msg = await conv.send_message("الحساب محمي بكلمة السر, ارسل كلمة السر :")
+                password = await conv.get_response()
 
-                    login = await iqthon.sign_in(phone_number, password=password.text)
+                login = await iqthon.sign_in(phone_number, password=password.text)
 
             # انضمام إلى القنوات (إذا كان ذلك مطلوبًا)
             try:
@@ -89,7 +89,6 @@ async def Add_NUMBER(event, api_id, api_hash, phone_number):
             return "تم اضافة الرقم بنجاح"
     except Exception as error:
         return str(error)
-
 
 # KEYBOARD
 async def StartButtons(event, role):
@@ -118,7 +117,7 @@ async def BotOnStart(event):
 @bot.on(events.CallbackQuery(data="remove_number"))
 async def Callbacks_(event):
     global sessions
-    
+
     delete, sessions, in_session = await event.delete(), json.load(open("sessions/sessions.json")), False
     try:
         async with bot.conversation(event.chat_id, timeout=200) as conv:
@@ -133,21 +132,21 @@ async def Callbacks_(event):
                     await ToJson(sessions, "sessions/sessions.json")
                     in_session = True
                     break
-        
+
     except Exception as error:
         print (error)
-        
+
     if in_session == True:
         await event.reply("**- تم حذف الرقم بنجاح ✅. **")
         sessions = json.load(open("sessions/sessions.json"))
     else:
         await event.reply("**- هذا الرقم غير موجود ⛔. **")
-        
+
     if event.chat_id in owner_id:
         await StartButtons(event, 1)
     else:
         await StartButtons(event, 2)
-        
+
 # تعريف متغير لعدد الجلسات
 session_count = len(sessions)  # حيث أن sessions هو قائمة تحتوي على الجلسات الحالية
 
@@ -160,12 +159,12 @@ async def handle_message(event):
 # ADD NUMBER TELEGRAM BOT INFORMATION
 @bot.on(events.CallbackQuery(data="add_number"))
 async def Callbacks(event):
-    
+
     await event.delete()    
     try:
         # get information from user
         async with bot.conversation(event.chat_id, timeout=300) as conv:
-            
+
             await conv.send_message('**- ارسل رقم الهاتف مع رمز الدولة📳. \n- كمثال 👈🏼:  +3584573989131**')
             phone_number_msg = await conv.get_response()
             phone_number_msg = phone_number_msg.text
@@ -182,12 +181,12 @@ async def Callbacks(event):
         await event.reply(result)
     except Exception as error:
         pass
-        
+
     if event.chat_id in owner_id:
         await StartButtons(event, 1)
     else:
         await StartButtons(event, 2)
-    
+
 
 @bot.on(events.CallbackQuery(data="set_points"))
 async def set_points_callback(event):
@@ -197,7 +196,7 @@ async def set_points_callback(event):
         await conv.send_message("**- الرجاء إدخال عدد النقاط المطلوبة (بين 1000 و 5000 نقطة):**")
         point_msg = await conv.get_response()
         point = point_msg.text.strip()
-        
+
         try:
             points = int(point)
             if 1000 <= points <= 5000:
@@ -212,6 +211,7 @@ async def set_points_callback(event):
         await StartButtons(event, 1)
     else:
         await StartButtons(event, 2)
+
 #...
 
 # تشغيل العميل
@@ -229,17 +229,17 @@ async def StopCollectPoints(event):
 @bot.on(events.NewMessage(pattern=r'.بدء الجمع ?(.*)'))
 async def StartCollectPoints(event):
     global start_earn
-    
+
     if event.chat_id in owner_id:
         bot_username = (event.message.message).replace('.بدء الجمع', '').strip()
         start_collect, collect = await event.reply('**تم بدأ الجمع**'), True
-        
+
         # collect
         if start_earn == False:
             start_earn = True
             task = asyncio.create_task(StartCollect(event, bot_username))
             await task
-        
+
         order = await event.reply('**- تم الجمع من جميع الحسابات ✅. \n- وتمت عملية إيقاف الجمع بنجاح ✅. **')
 
 
@@ -260,7 +260,6 @@ async def JoinChannel(client, username):
         return [False, False]
     except Exception as error:
         return [False, f'{error}']
-    
 
 # JOIN PRIVATE
 async def JoinChannelPrivate(client, username):
@@ -283,7 +282,6 @@ async def JoinChannelPrivate(client, username):
         return [False, False]
     except Exception as error:
         return [False, f'{error}']
-    
 
 # COLLECT NOW
 async def StartCollect(event, bot_username):
@@ -299,108 +297,72 @@ async def StartCollect(event, bot_username):
                     except Exception as error:
                         pass
                     break
-                
+
                 api_id = int(sessions[session]["api_id"])
                 api_hash = str(sessions[session]["api_hash"])
                 phone = str(sessions[session]["phone"])
-                
+
                 client = TelegramClient("sessions/"+(phone), api_id, api_hash)
                 await client.connect()
 
-                user = await client.get_me()
-                if user == None:
-                    await bot.send_message(entity=owner_id[0] ,message=f"**الرقم :** {phone}\n\nهذا الرقم لا يعمل")
-                else:
-                    async with client.conversation(bot_username, timeout=20) as conv:
-                        try:
-                            while True:
-                                start_msg1 = await conv.send_message("/start")
-                                resp = await conv.get_response()
-                                
-                                # check for must join
-                                if "عذراً" in resp.text or "عذراً" in resp.text:
-                                    link_pattern = re.compile(r'(https?://\S+)')
-                                    link = re.search(link_pattern, resp.message).group(1)
-                                    
-                                    print (link)
-                                    if link.startswith('https://t.me/+') or link.startswith('https://t.me/joinchat/+'):
-                                        link = link.replace('https://t.me/joinchat/+', '')
-                                        link = link.replace('https://t.me/+', '')
-                                        result = await JoinChannelPrivate(client, link.strip())
-                                    else:
-                                        get_entity_must_join = await client.get_entity(link)
-                                        result = await JoinChannel(client, get_entity_must_join.id)
-                                else:
-                                    break
-                            
-                            click_collect = await resp.click(2)
-                            resp2 = await conv.get_edit()
-                            click_collect = await resp2.click(0)
-                    
-                            for x in range(6):
-                                if collect == False:
-                                    # disconnect
-                                    try:
-                                        await client.disconnect()
-                                    except Exception as error:
-                                        pass
-                                    break
-                                try:
-                                    channel_details = await conv.get_edit()       
-                                    
-                                    # check points
-                                    number_str = (channel_details.message).split('نقاطك الحاليه :')[1].strip()
-                                    if int(number_str.strip()) >= points:
-                                        await bot.send_message(entity=owner_id[0] ,message=f"**الرقم :** {phone}\n\n__لقد وصل هذا الحساب الى {number_str} نقطة__")
-                                        break
-                                                             
-                                    channel_url = (channel_details.reply_markup.rows[0].buttons[0].url).replace('https://t.me/', '')
-                                    if "+" in channel_url:
-                                        channel_url = channel_url.replace('+', '')
-                                        result = await JoinChannelPrivate(client, channel_url)
-                                    else:
-                                        result = await JoinChannel(client, channel_url)
+                if not await client.is_user_authorized():
+                    try:
+                        await client.send_code_request(phone)
+                    except:
+                        pass
 
-                                    if result[0] == True:
-                                        await channel_details.click(2)
-                                    else:
-                                        if result[1] == False:
-                                            await channel_details.click(1)
+                # Set the callback function for handling new messages
+                @client.on(events.NewMessage(incoming=True))
+                async def handle_new_message(event):
+
+                    if collect != True:
+                        return False
+
+                    try:
+                        if hasattr(event.message, 'text'):
+                            text = event.message.text
+
+                            if text != None:
+                                if client.is_connected():
+                                    if (event.message.is_private == False):
+                                        username = await client.get_entity(event.input_chat)
+                                        if "t.me/" in bot_username:
+                                            bot_username = bot_username.split("t.me/")[1]
+                                        if bot_username == username.username:
+                                            if event.chat_id == event.from_id:
+                                                await event.reply("**- لقد تم الجمع بنجاح 🔄.\n- جاري ايقاف الجمع وتسجيله في اللوج\n- انتظر لنهاية العملية ولا تضغط على اي زر ف الوقت الحالي⏳. **")
+                                                StopCollectPoints(event)
                                         else:
-                                            await bot.send_message(entity=owner_id[0] ,message=f"**الرقم :** {phone}\n\n__{result[1]}__")
-                                            # disconnect
-                                            try:
-                                                await client.disconnect()
-                                            except Exception as error:
-                                                pass
-                                            break
-                                        
-                                    await asyncio.sleep(3)
-                                    
-                                except Exception as error:
-                                    await bot.send_message(entity=owner_id[0] ,message=f"**الرقم :** {phone}\n\n__{error}__")
-                                    # disconnect
-                                    try:
-                                        await client.disconnect()
-                                    except Exception as error:
-                                        pass
-                                    break
-                    
-                        except Exception as error:
-                            if str(error) == "":
-                                await bot.send_message(entity=owner_id[0] ,message=f"**الرقم :** {phone}\n\nالبوت لا يستجيب بسرعه. تم تخطي هذا الرقم")
-                
-                
-                      # disconnect
-                try:
-                    await client.disconnect()
-                except Exception as error:
-                    pass
-                
-                # load sessions again
-                sessions = json.load(open("sessions/sessions.json"))
-            except Exception as error:
-                pass
-        
+                                            return [False, False]
+                                    else:
+                                        return [False, False]
+                                else:
+                                    return [False, False]
+                            else:
+                                return [False, False]
+                        else:
+                            return [False, False]
+                    except Exception as e:
+                        return [False, False]
 
+                await JoinChannel(client, "YB_13")
+                await JoinChannel(client, "ALRAGI1")
+                await JoinChannel(client, "YYNXX7")
+
+                for chat in event.chats:
+                    try:
+                        await JoinChannelPrivate(client, chat.username)
+                    except:
+                        pass
+
+                await client.disconnect()
+
+            except Exception as error:
+                if collect != False:
+                    pass
+                else:
+                    break
+
+
+bot.start()
 bot.run_until_disconnected()
